b0ed4100a08c39157c1a0ccef4a2f2699a87b9c0
Dobromir <dobromirtodoroff@gmail.com>
2018-07-13 19:26:10 +0300

    test

--- www/js/index.js
+++ www/js/index.js
@@ -41,13 +41,13 @@ var app = {
 
             var notificationOpenedCallBack = function(jsonData) {
                 console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-                alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+                // alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
             };            
 
             window.plugins.OneSignal
                 .startInit('adfdf8db-3cbf-46c6-b574-9a9a60ca41d3')
                 .handleNotificationReceived(function(jsonData) {
-                    alert("Notification received:\n" + JSON.stringify(jsonData));
+                    // alert("Notification received:\n" + JSON.stringify(jsonData));
                     console.log('Did I receive a notification: ' + JSON.stringify(jsonData));
                   })
                 .handleNotificationOpened(notificationOpenedCallBack)
