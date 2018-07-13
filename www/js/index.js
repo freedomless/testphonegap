b47fd8351bab2fb7438fceb3fe4af62872cfa8fb
Dobromir <dobromirtodoroff@gmail.com>
2018-07-13 19:11:27 +0300

    test

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        // app.receivedEvent('deviceready');
         
         // Add to index.js or the first page that loads with your app.
         // For Intel XDK and please add this to your app.js
